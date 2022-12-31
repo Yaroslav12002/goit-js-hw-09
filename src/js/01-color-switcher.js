de83924e25f25274f33f4f7de657449aeb69dce3
Yaroslav12002 <my.mail12002@gmail.com>
2022-12-31 16:25:13 +0200

    task 1 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let bodyBackgroundTimerID = null;
+let isBodyBackgroundIntervalWorks = false;
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  if (isBodyBackgroundIntervalWorks) {
+    return;
+  }
+
+  bodyBackgroundTimerID = setInterval(changeBodyBackground, 1000);
+  isBodyBackgroundIntervalWorks = true;
+  console.log('Started');
+}
+
+function onStopBtnClick() {
+  console.log('Stoppped');
+  clearInterval(bodyBackgroundTimerID);
+  isBodyBackgroundIntervalWorks = false;
+}
+
+function changeBodyBackground() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
